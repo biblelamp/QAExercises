25603036e2aa35f0ca6dca168be6c66dab9d645b
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-16 14:33:54 +0300

    test to admin privilege added FirefoxDriver() opt

--- performance-lab.ru/MatrixBoard/src/test/java/MatrixBoardAdminTest.java
+++ performance-lab.ru/MatrixBoard/src/test/java/MatrixBoardAdminTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -22,7 +23,7 @@ public class MatrixBoardAdminTest {
     @Before
     public void setUp() {
         System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\geckodriver.exe");
-        driver = new FirefoxDriver();
+        driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
@@ -47,7 +48,7 @@ public class MatrixBoardAdminTest {
 
         WebElement adduserButton = driver.findElement(By.id("add-person"));
 
-        if (adduserButton.isDisplayed() == false)
+        if (adduserButton.isDisplayed() == true)
             System.out.println("I have admin privilege =)");
         else
             System.out.println("I do not have admin privilege =(");
