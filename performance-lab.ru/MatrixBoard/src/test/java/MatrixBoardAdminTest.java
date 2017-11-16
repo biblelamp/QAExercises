c805632966c1d65e8258558593316a4d519a3e2d
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-16 14:27:45 +0300

    added test to admin privilege

---
+++ performance-lab.ru/MatrixBoard/src/test/java/MatrixBoardAdminTest.java
@@ -0,0 +1,55 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Sergej Irupin on 16.11.17.
+ */
+public class MatrixBoardAdminTest {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    static final String USER_NAME = "admin";
+    static final String PASSWORD = "admin";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void loginTest() {
+
+        driver.navigate().to("http://at.pflb.ru/matrixboard2/"); // перейти по URL
+
+        WebElement loginField = driver.findElement(By.id("login-username"));
+        WebElement passwordField = driver.findElement(By.id("login-password"));
+        WebElement submitBtn = driver.findElement(By.id("login-button"));
+
+        loginField.sendKeys(USER_NAME);
+        passwordField.sendKeys(PASSWORD);
+        submitBtn.click();
+
+        WebElement adduserButton = driver.findElement(By.id("add-person"));
+
+        if (adduserButton.isDisplayed() == false)
+            System.out.println("I have admin privilege =)");
+        else
+            System.out.println("I do not have admin privilege =(");
+    }
+}
