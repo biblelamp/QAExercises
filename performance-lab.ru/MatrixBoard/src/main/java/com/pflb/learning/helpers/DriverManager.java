73e36224bdf5ebe1d3f1fc82e5067cdca4d092d3
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-17 09:52:39 +0300

    added parameter in constructor FirefoxDriver()

--- performance-lab.ru/MatrixBoard/src/main/java/com/pflb/learning/helpers/DriverManager.java
+++ performance-lab.ru/MatrixBoard/src/main/java/com/pflb/learning/helpers/DriverManager.java
@@ -2,6 +2,7 @@ package com.pflb.learning.helpers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverManager {
     private static DriverManager instance;
@@ -12,7 +13,7 @@ public class DriverManager {
     public static WebDriver getDriver() {
         if (instance == null) {
             instance = new DriverManager();
-            instance.driver = new FirefoxDriver();
+            instance.driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         }
         return instance.driver;
     }
