9e6130f2c5eb508b034a3f5def344cc1686673eb
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-26 10:24:16 +0300

    java final task junit tests

--- performance-lab.ru/FinalTask/src/test/java/calc/AppTest.java
+++ performance-lab.ru/FinalTask/src/test/java/calc/AppTest.java
@@ -1,38 +1,37 @@
 package calc;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
- * Unit test for simple App.
+ * Java. Final task: homework
+ * Unit test for simple calculator
+ *
+ * @author Sergey Iryupin
+ * @version 0.1 dated Nov 26, 2017
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import tools.Utilities;
+
+public class AppTest {
+    private Utilities calc;
+
+    @Before
+    public void startTest() {
+        calc = new Utilities();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testNormalDiv() {
+        Assert.assertEquals(2.5, calc.divide(5, 2), 0);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testDivByZero() {
+        Assert.assertEquals(Double.POSITIVE_INFINITY, calc.divide(4, 0), 0);
+    }
+ 
+    @Test
+    public void testDivNaN() {
+        Assert.assertEquals(Double.NaN, calc.divide(0, 0), 0);
     }
 }
