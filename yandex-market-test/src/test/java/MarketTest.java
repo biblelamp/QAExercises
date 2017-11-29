497889e23e5037c520372c1d026b47e8347ddff6
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-29 16:19:30 +0300

    class MarketTest second version fix a bit

--- yandex-market-test/src/test/java/MarketTest.java
+++ yandex-market-test/src/test/java/MarketTest.java
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
  * Test class for market.yandex.ru
  *
  * @author Sergey Irupin
- * @version 0.2 dated Nov 29, 2017
+ * @version 0.2.1 dated Nov 29, 2017
  */
 public class MarketTest {
     static WebDriver driver;
@@ -39,14 +39,16 @@ public class MarketTest {
     @Test
     public void laptopMarketTest() {
         Page pageNotebooks = new Page(driver);
-        pageNotebooks.maximize();
-        pageNotebooks.go(new String[]{"http://yandex.ru/", "Маркет", "Компьютеры", "Ноутбуки"});
-
-        pageNotebooks.setPriceById("glf-priceto-var", "30000");
-        pageNotebooks.selectCheckByXPathAndId("//label[@for='glf-7893318-152722']", "glf-7893318-152722");
-        pageNotebooks.selectCheckByXPathAndId("//label[@for='glf-7893318-152981']", "glf-7893318-152722");
-        pageNotebooks.clickButtonByXpath(
-                "//div[@class='n-filter-panel-aside__apply']//button[.='Применить']", 10);
+        pageNotebooks.maximize()
+                .load("http://yandex.ru/")
+                .goByLinkText("Маркет")
+                .goByLinkText("Компьютеры")
+                .goByLinkText("Ноутбуки")
+                .setPriceById("glf-priceto-var", "30000")
+                .selectCheckById("glf-7893318-152722")
+                .selectCheckById("glf-7893318-152981")
+                .clickButtonByXpath(
+                        "//div[@class='n-filter-panel-aside__apply']//button[.='Применить']", 10);
 
         List<WebElement> list = driver.findElements(By.xpath("//div[@class='n-snippet-card2__title']"));
         String notebookName = list.get(0).getText();
@@ -63,17 +65,19 @@ public class MarketTest {
     @Test
     public void tabletMarketTest() {
         Page pageTablet = new Page(driver);
-        pageTablet.maximize();
-        pageTablet.go(new String[]{"http://yandex.ru/", "Маркет", "Компьютеры", "Планшеты"});
-
-        pageTablet.clickButtonByXpath(
-                "//div[@class='n-filter-panel-aside__content']//button[.='Показать всё']", 0);
-        pageTablet.selectCheckByXPathAndId("//label[@for='glf-7893318-267101']", "glf-7893318-267101");
-        pageTablet.selectCheckByXPathAndId("//label[@for='glf-7893318-153080']", "glf-7893318-153080");
-        pageTablet.setPriceById("glf-pricefrom-var", "20000");
-        pageTablet.setPriceById("glf-priceto-var", "25000");
-        pageTablet.clickButtonByXpath(
-                "//div[@class='n-filter-panel-aside__apply']//button[.='Применить']", 10);
+        pageTablet.maximize()
+                .load("http://yandex.ru/")
+                .goByLinkText("Маркет")
+                .goByLinkText("Компьютеры")
+                .goByLinkText("Планшеты")
+                .clickButtonByXpath(
+                    "//div[@class='n-filter-panel-aside__content']//button[.='Показать всё']", 0)
+                .selectCheckById("glf-7893318-267101")
+                .selectCheckById("glf-7893318-153080")
+                .setPriceById("glf-pricefrom-var", "20000")
+                .setPriceById("glf-priceto-var", "25000")
+                .clickButtonByXpath(
+                    "//div[@class='n-filter-panel-aside__apply']//button[.='Применить']", 10);
 
         List<WebElement> list = driver.findElements(By.xpath("//div[@class='n-snippet-card2__title']"));
         String tabletName = list.get(0).getText();
