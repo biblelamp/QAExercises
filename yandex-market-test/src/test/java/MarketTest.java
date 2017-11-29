5c908a4368ab42c2a8b8bc5bb10f3e953da3139d
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-29 08:18:08 +0300

    class MarketTest second version

--- yandex-market-test/src/test/java/MarketTest.java
+++ yandex-market-test/src/test/java/MarketTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.Page;
 import tools.DriverManager;
 import tools.Tools;
 
@@ -16,7 +17,7 @@ import java.util.concurrent.TimeUnit;
  * Test class for market.yandex.ru
  *
  * @author Sergey Irupin
- * @version dated Nov 28, 2017
+ * @version 0.2 dated Nov 29, 2017
  */
 public class MarketTest {
     static WebDriver driver;
@@ -27,7 +28,7 @@ public class MarketTest {
         System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\geckodriver.exe");
         driver = DriverManager.getDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, 60);
     }
 
     @AfterClass
@@ -37,35 +38,22 @@ public class MarketTest {
 
     @Test
     public void laptopMarketTest() {
-        driver.manage().window().maximize();
-        driver.get("http://yandex.ru/");
-        driver.findElement(By.linkText("Маркет")).click();
-        driver.findElement(By.linkText("Компьютеры")).click();
-        driver.findElement(By.linkText("Ноутбуки")).click();
+        Page pageNotebooks = new Page(driver);
+        pageNotebooks.maximize();
+        pageNotebooks.go(new String[]{"http://yandex.ru/", "Маркет", "Компьютеры", "Ноутбуки"});
 
-        // extend parameters
-        driver.findElement(By.id("glf-priceto-var")).click();
-        driver.findElement(By.id("glf-priceto-var")).clear();
-        driver.findElement(By.id("glf-priceto-var")).sendKeys("30000");
-        driver.findElement(By.xpath("//label[@for='glf-7893318-152722']")).click();
-        if (!driver.findElement(By.id("glf-7893318-152722")).isSelected()) {
-            driver.findElement(By.id("glf-7893318-152722")).click();
-        }
-        driver.findElement(By.xpath("//label[@for='glf-7893318-152981']")).click();
-        if (!driver.findElement(By.id("glf-7893318-152981")).isSelected()) {
-            driver.findElement(By.id("glf-7893318-152981")).click();
-        }
-        driver.findElement(
-                By.xpath("//div[@class='n-filter-panel-aside__apply']//button[.='Применить']")).click();
-        Tools.sleep(10); // waiting
+        pageNotebooks.setPriceById("glf-priceto-var", "30000");
+        pageNotebooks.selectCheckByXPathAndId("//label[@for='glf-7893318-152722']", "glf-7893318-152722");
+        pageNotebooks.selectCheckByXPathAndId("//label[@for='glf-7893318-152981']", "glf-7893318-152722");
+        pageNotebooks.clickButtonByXpath(
+                "//div[@class='n-filter-panel-aside__apply']//button[.='Применить']", 10);
 
         List<WebElement> list = driver.findElements(By.xpath("//div[@class='n-snippet-card2__title']"));
         String notebookName = list.get(0).getText();
         System.out.println(list.size());
 
-        driver.findElement(By.id("header-search")).sendKeys(notebookName);
-        driver.findElement(By.xpath("//span[@class='search2__button']//button[.='Найти']")).click();
-        Tools.sleep(10); // waiting
+        pageNotebooks.search(
+                notebookName, "header-search", "//span[@class='search2__button']//button[.='Найти']", 10);
 
         WebElement title = driver.findElement(By.xpath("//div[@class='n-title__text']"));
         Assert.assertEquals(notebookName, title.getText());
@@ -74,41 +62,25 @@ public class MarketTest {
 
     @Test
     public void tabletMarketTest() {
-        //driver.manage().window().maximize();
-        driver.get("http://yandex.ru/");
-        driver.findElement(By.linkText("Маркет")).click();
-        driver.findElement(By.linkText("Компьютеры")).click();
-        driver.findElement(By.linkText("Планшеты")).click();
+        Page pageTablet = new Page(driver);
+        pageTablet.maximize();
+        pageTablet.go(new String[]{"http://yandex.ru/", "Маркет", "Компьютеры", "Планшеты"});
 
-        // extend parameters
-        driver.findElement(
-                By.xpath("//div[@class='n-filter-panel-aside__content']//button[.='Показать всё']")).click();
-        driver.findElement(By.xpath("//label[@for='glf-7893318-267101']")).click();
-        if (!driver.findElement(By.id("glf-7893318-267101")).isSelected()) {
-            driver.findElement(By.id("glf-7893318-267101")).click();
-        }
-        driver.findElement(By.xpath("//label[@for='glf-7893318-153080']")).click();
-        if (!driver.findElement(By.id("glf-7893318-153080")).isSelected()) {
-            driver.findElement(By.id("glf-7893318-153080")).click();
-        }
-        driver.findElement(By.id("glf-pricefrom-var")).click();
-        driver.findElement(By.id("glf-pricefrom-var")).clear();
-        driver.findElement(By.id("glf-pricefrom-var")).sendKeys("20000");
-        driver.findElement(By.id("glf-priceto-var")).click();
-        driver.findElement(By.id("glf-priceto-var")).clear();
-        driver.findElement(By.id("glf-priceto-var")).sendKeys("25000");
-
-        driver.findElement(
-                By.xpath("//div[@class='n-filter-panel-aside__apply']//button[.='Применить']")).click();
-        Tools.sleep(10); // waiting
+        pageTablet.clickButtonByXpath(
+                "//div[@class='n-filter-panel-aside__content']//button[.='Показать всё']", 0);
+        pageTablet.selectCheckByXPathAndId("//label[@for='glf-7893318-267101']", "glf-7893318-267101");
+        pageTablet.selectCheckByXPathAndId("//label[@for='glf-7893318-153080']", "glf-7893318-153080");
+        pageTablet.setPriceById("glf-pricefrom-var", "20000");
+        pageTablet.setPriceById("glf-priceto-var", "25000");
+        pageTablet.clickButtonByXpath(
+                "//div[@class='n-filter-panel-aside__apply']//button[.='Применить']", 10);
 
         List<WebElement> list = driver.findElements(By.xpath("//div[@class='n-snippet-card2__title']"));
         String tabletName = list.get(0).getText();
         System.out.println(list.size());
 
-        driver.findElement(By.id("header-search")).sendKeys(tabletName);
-        driver.findElement(By.xpath("//span[@class='search2__button']//button[.='Найти']")).click();
-        Tools.sleep(10); // waiting
+        pageTablet.search(
+                tabletName, "header-search", "//span[@class='search2__button']//button[.='Найти']", 10);
 
         WebElement title = driver.findElement(By.xpath("//div[@class='n-title__text']"));
         Assert.assertEquals(tabletName, title.getText());
